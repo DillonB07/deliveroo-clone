665d31182a5704f72d6ff80fc68751375978c892
DillonB07 <dillonbarnes07@gmail.com>
2022-07-11 19:43:59 +0100

    Setup Redux

--- App.js
+++ App.js
@@ -3,18 +3,22 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { TailwindProvider } from "tailwindcss-react-native";
 import RestaurantScreen from "./screens/RestaurantScreen";
 import HomeScreen from "./screens/HomeScreen";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./store";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-        </Stack.Navigator>
-      </TailwindProvider>
+      <ReduxProvider store={store}>
+        <TailwindProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+          </Stack.Navigator>
+        </TailwindProvider>
+      </ReduxProvider>
     </NavigationContainer>
   );
 }
