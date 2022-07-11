e36a8fbc641cbd6cbde7ccb471fe556ec0ffba6d
DillonB07 <dillonbarnes07@gmail.com>
2022-07-11 16:45:19 +0100

    Add featured rows

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -15,6 +15,7 @@ import {
   AdjustmentsIcon,
 } from "react-native-heroicons/outline";
 import Categories from "../components/Categories";
+import FeaturedRow from "../components/FeaturedRow";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -64,6 +65,24 @@ const HomeScreen = () => {
         <Categories />
 
         {/* Featured Rows */}
+        {/* Featured */}
+        <FeaturedRow
+          id="1"
+          title="Featured"
+          description="Paid placements from our partners"
+        />
+        {/* Tasty Discounts */}
+        <FeaturedRow
+          id="2"
+          title="Tasty Discounts"
+          description="Everyone's been enjoying these juicy discounts"
+        />
+        {/* Offers near you */}
+        <FeaturedRow
+          id="3"
+          title="Offers near you!"
+          description="Why not support your local restaurant tonight!"
+        />
       </ScrollView>
     </SafeAreaView>
   );
