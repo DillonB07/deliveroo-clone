1d961bdabb4d972f4935697b510d452e2015ba81
DillonB07 <dillonbarnes07@gmail.com>
2022-07-11 19:29:20 +0100

    Add menu

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -78,6 +78,18 @@ const RestaurantScreen = () => {
       <View>
         <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
       </View>
+
+      {/* Dish rows */}
+      {dishes?.map((dish) => (
+        <DishRow
+          key={dish?._id}
+          id={dish?._id}
+          name={dish?.name}
+          description={dish?.short_description}
+          price={dish?.price}
+          image={dish?.image}
+        />
+      ))}
     </ScrollView>
   );
 };
